659f248dd03668695aa5b1fc8e75c2722f0fdc8d
ELAKKIYAPACHAGOUNDAN <elakkiya0915@gmail.com>
2025-11-02 21:39:44 +0530

    adding notes - oops

--- OOPS/Notes.java
+++ OOPS/Notes.java
@@ -5,8 +5,16 @@ public class Notes{
         Default value : String : null || int : 0(zero)
 
         1) Encapsulation:
-        ->Encapsulation having an important part (security) access by indirectly.
+        ->Encapsulation is the process of wrapping variables (data) and methods together inside a class.
+        ->It enables data hiding, ensuring that sensitive information is not directly accessible from outside the class.
+        ->We are restricting the direct access to the variables using access modifiers like private, public, and protected, we control how data is accessed or modified.
+        ->To modify these private variables safely, we use getter and setter methods.
+        ->The number of getter and setter methods depends on how many variables we want to protect.
 
+        -> Real-time example:
+        The wallet is the class.
+        Your cash and cards are the private data.
+        
         2) Inheritance:
 
         3) Abstraction:
